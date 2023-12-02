0dd45cbecb60fa51207e4f14261a4d61700750d3
Ingemar Bjorn Elfstrom <ingemar.elfstrom@alfalaval.com>
2023-12-02 14:02:10 +0100

    Config and infra for metrics and logging

--- API/Extensions/HostingExtensions.cs
+++ API/Extensions/HostingExtensions.cs
@@ -1,7 +1,11 @@
 using API.Middlewares;
 using FindMyCar.Core.Data;
 using Microsoft.EntityFrameworkCore;
-
+using Prometheus;
+using Serilog;
+using Serilog.AspNetCore;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.Grafana.Loki;
 namespace API.Extensions;
 
 public static class HostingExtensions
@@ -18,6 +22,14 @@ public static class HostingExtensions
         builder.Services.AddDbContext<VehicleContext>(options => {
             options.UseSqlServer(builder.Configuration.GetConnectionString("FindMyCar"));
         });
+        builder.Host.UseSerilog((ctx,cfg)=>
+        {
+            //Override Few of the Configurations
+            cfg.Enrich.WithProperty("Application", ctx.HostingEnvironment.ApplicationName)
+                .Enrich.WithProperty("Environment", ctx.HostingEnvironment.EnvironmentName)
+                .WriteTo.Console(new RenderedCompactJsonFormatter())
+                .WriteTo.GrafanaLoki(ctx.Configuration["Loki"]);
+        });   
         return builder;
     }
 
@@ -32,9 +44,30 @@ public static class HostingExtensions
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-        app.UseHttpsRedirection();
+        app.UseHttpsRedirection();     
+        app.ConfigureMetrics();
         app.UseMiddleware<ExceptionMiddleware>();
         app.MapControllers();
         return app;
     }
+
+    /// <summary>
+    /// Configure metrics for WebApplication
+    /// </summary>
+    /// <param name="app">WebApplication to configure metrics for.</param>
+    public static WebApplication ConfigureMetrics(this WebApplication app)
+    {
+        var counter = Metrics.CreateCounter("api_path_counter", "Counts requests to the API endpoints", new CounterConfiguration
+        {
+            LabelNames = new[] { "method", "endpoint" }
+        });
+        app.Use((context, next) =>
+        {
+            counter.WithLabels(context.Request.Method, context.Request.Path).Inc();
+            return next();
+        });
+        app.UseMetricServer();
+        app.UseHttpMetrics();    
+        return app;
+    }    
 }
