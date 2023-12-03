079ca28fe1f278aabfaf50efc35bb85a28722ce3
Ingemar Bjorn Elfstrom <ingemar.elfstrom@alfalaval.com>
2023-12-03 17:55:49 +0100

    Updated search view

--- client/src/app/components/vehicle-search/vehicle-list/vehicle-list.component.ts
+++ client/src/app/components/vehicle-search/vehicle-list/vehicle-list.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from "@angular/common";
 import { Vehicle } from "@interfaces/Vehicle";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-vehicle-list",
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatListModule],
+  imports: [CommonModule, MatIconModule, MatListModule, MatExpansionModule, MatFormFieldModule, RouterModule],
   templateUrl: "./vehicle-list.component.html",
   styleUrl: "./vehicle-list.component.scss",
 })
